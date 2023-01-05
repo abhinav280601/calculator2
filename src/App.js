1f4b7b6c44e2239de4bb1bd12458c40a52be4ae2
radhikarungta <radhika.rr2002@gmail.com>
2023-01-05 15:50:07 +0530

    design changes

--- src/App.js
+++ src/App.js
@@ -76,73 +76,75 @@ function App() {
   };
 
   return (
-    <div className="container">
-      <div className="wrapper">
-        <div className="screen">
-          <div className="input_container">
-            {input !== "" || input === "0" ? (
-              <NumericFormat
-                value={input}
-                displayType={"text"}
-                thousandSeparator={true}
-              />
-            ) : (
-              <NumericFormat
-                value={preState}
-                displayType={"text"}
-                thousandSeparator={true}
-              />
-            )}
+    <div className="u_container">
+      <div className="container">
+        <div className="wrapper">
+          <div className="screen">
+            <div className="input_container">
+              {input !== "" || input === "0" ? (
+                <NumericFormat
+                  value={input}
+                  displayType={"text"}
+                  thousandSeparator={true}
+                />
+              ) : (
+                <NumericFormat
+                  value={preState}
+                  displayType={"text"}
+                  thousandSeparator={true}
+                />
+              )}
+            </div>
           </div>
-        </div>
 
-        <div className="btn" onClick={inputNum}>
-          7
-        </div>
-        <div className="btn" onClick={inputNum}>
-          8
-        </div>
-        <div className="btn" onClick={inputNum}>
-          9
-        </div>
-        <div className="btn orange" onClick={reset}>
-          C
-        </div>
-        <div className="btn" onClick={inputNum}>
-          4
-        </div>
-        <div className="btn" onClick={inputNum}>
-          5
-        </div>
-        <div className="btn" onClick={inputNum}>
-          6
-        </div>
-        <div className="btn" onClick={operatorType}>
-          -
-        </div>
-        <div className="btn" onClick={inputNum}>
-          1
-        </div>
-        <div className="btn" onClick={inputNum}>
-          2
-        </div>
-        <div className="btn" onClick={inputNum}>
-          3
-        </div>
-        <div className="btn" onClick={operatorType}>
-          +
-        </div>
-        <div className="btn light-gray" onClick={operatorType}>
-          /
-        </div>
-        <div className="btn zero" onClick={inputNum}>
-          0
-        </div>
-        <div className="btn" onClick={operatorType}>
-          x
-        </div>
-        <div className="btn" onClick={equals}>
-          =
+          <div className="btn" onClick={inputNum}>
+            7
+          </div>
+          <div className="btn" onClick={inputNum}>
+            8
+          </div>
+          <div className="btn" onClick={inputNum}>
+            9
+          </div>
+          <div className="btn orange" onClick={reset}>
+            C
+          </div>
+          <div className="btn" onClick={inputNum}>
+            4
+          </div>
+          <div className="btn" onClick={inputNum}>
+            5
+          </div>
+          <div className="btn" onClick={inputNum}>
+            6
+          </div>
+          <div className="btn" onClick={operatorType}>
+            -
+          </div>
+          <div className="btn" onClick={inputNum}>
+            1
+          </div>
+          <div className="btn" onClick={inputNum}>
+            2
+          </div>
+          <div className="btn" onClick={inputNum}>
+            3
+          </div>
+          <div className="btn" onClick={operatorType}>
+            +
+          </div>
+          <div className="btn light-gray" onClick={operatorType}>
+            <span>&#247;</span>
+          </div>
+          <div className="btn zero" onClick={inputNum}>
+            0
+          </div>
+          <div className="btn" onClick={operatorType}>
+            x
+          </div>
+          <div className="btn" onClick={equals}>
+            =
+          </div>
         </div>
       </div>
     </div>
