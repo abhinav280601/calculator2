a2c2b6ee0b20bdfbcdadde567dee28ca64030669
radhikarungta <radhika.rr2002@gmail.com>
2023-01-11 15:09:28 +0530

    new updates

--- src/App.js
+++ src/App.js
@@ -80,81 +80,81 @@ function App() {
   return (
     <div className="u_container">
       <div className="container">
-        <ReactCardFlip isFlipped={flip} flipDirection="vertical">
-          <div className="wrapper">
-            <div className="screen">
-              <div className="input_container">
-                <div class="lcd-background">888888888888</div>
-                {input !== "" || input === "0" ? (
-                  <NumericFormat
-                    value={input}
-                    displayType={"text"}
-                    thousandSeparator={false}
-                  />
-                ) : (
-                  <NumericFormat
-                    value={preState}
-                    displayType={"text"}
-                    thousandSeparator={false}
-                  />
-                )}
-              </div>
+        {/* <ReactCardFlip isFlipped={flip} flipDirection="vertical"> */}
+        <div className="wrapper">
+          <div className="screen">
+            <div className="input_container">
+              <div class="lcd-background">888888888888</div>
+              {input !== "" || input === "0" ? (
+                <NumericFormat
+                  value={input}
+                  displayType={"text"}
+                  thousandSeparator={false}
+                />
+              ) : (
+                <NumericFormat
+                  value={preState}
+                  displayType={"text"}
+                  thousandSeparator={false}
+                />
+              )}
             </div>
-
-            <div className="btn" onClick={inputNum}>
-              7
-            </div>
-            <div className="btn" onClick={inputNum}>
-              8
-            </div>
-            <div className="btn" onClick={inputNum}>
-              9
-            </div>
-            <div className="btn orange" onClick={reset}>
-              C
-            </div>
-            <div className="btn" onClick={inputNum}>
-              4
-            </div>
-            <div className="btn" onClick={inputNum}>
-              5
-            </div>
-            <div className="btn" onClick={inputNum}>
-              6
-            </div>
-            <div className="btn" onClick={operatorType}>
-              -
-            </div>
-            <div className="btn" onClick={inputNum}>
-              1
-            </div>
-            <div className="btn" onClick={inputNum}>
-              2
-            </div>
-            <div className="btn" onClick={inputNum}>
-              3
-            </div>
-            <div className="btn" onClick={operatorType}>
-              +
-            </div>
-            <div className="btn" onClick={operatorType}>
-              /
-            </div>
-            <div className="btn zero" onClick={inputNum}>
-              0
-            </div>
-            <div className="btn" onClick={operatorType}>
-              x
-            </div>
-            <div className="btn" onClick={equals}>
-              =
-            </div>
-            <button className="btn history" onClick={() => setFlip(!flip)}>
-              History
-            </button>
           </div>
 
-          <div className="container_flip">
+          <div className="btn" onClick={inputNum}>
+            7
+          </div>
+          <div className="btn" onClick={inputNum}>
+            8
+          </div>
+          <div className="btn" onClick={inputNum}>
+            9
+          </div>
+          <div className="btn orange" onClick={reset}>
+            C
+          </div>
+          <div className="btn" onClick={inputNum}>
+            4
+          </div>
+          <div className="btn" onClick={inputNum}>
+            5
+          </div>
+          <div className="btn" onClick={inputNum}>
+            6
+          </div>
+          <div className="btn" onClick={operatorType}>
+            -
+          </div>
+          <div className="btn" onClick={inputNum}>
+            1
+          </div>
+          <div className="btn" onClick={inputNum}>
+            2
+          </div>
+          <div className="btn" onClick={inputNum}>
+            3
+          </div>
+          <div className="btn" onClick={operatorType}>
+            +
+          </div>
+          <div className="btn" onClick={operatorType}>
+            /
+          </div>
+          <div className="btn zero" onClick={inputNum}>
+            0
+          </div>
+          <div className="btn" onClick={operatorType}>
+            x
+          </div>
+          <div className="btn" onClick={equals}>
+            =
+          </div>
+          <button className="btn history" onClick={() => setFlip(!flip)}>
+            History
+          </button>
+        </div>
+
+        {/* <div className="container_flip">
             <div className="display_container">
               <p>HISTORY</p>
             </div>
@@ -162,7 +162,7 @@ function App() {
               back
             </button>
           </div>
-        </ReactCardFlip>
+        </ReactCardFlip> */}
       </div>
     </div>
   );
