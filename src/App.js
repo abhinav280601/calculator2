fea2e31e8de2d98d50dce4853dc79798acd7d1d6
radhikarungta <radhika.rr2002@gmail.com>
2023-01-14 16:41:24 +0530

    keydown feature added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,57 @@
 import "./App.css";
-import { NumericFormat } from "react-number-format";
-import { useState, useEffect } from "react";
+import { NumberFormatBase, NumericFormat } from "react-number-format";
+import { useState, useEffect, useCallback } from "react";
 import ReactCardFlip from "react-card-flip";
-
 function App() {
   const [flip, setFlip] = useState(false);
   const [preState, setPreState] = useState("");
   const [curState, setCurState] = useState("");
+  const [curStateKey, setCurStateKey] = useState("");
   const [input, setInput] = useState("0");
   const [operator, setOperator] = useState(null);
   const [total, setTotal] = useState(false);
 
+  const isNumberPressed = (key) => {
+    if (
+      key === "1" ||
+      key === "2" ||
+      key === "3" ||
+      key === "4" ||
+      key === "5" ||
+      key === "6" ||
+      key === "7" ||
+      key === "8" ||
+      key === "9" ||
+      key === "0" ||
+      key === "."
+    ) {
+      return true;
+    }
+  };
+
+  const isOperatorPressed = (key) => {
+    if (key === "+" || key === "-" || key === "*" || key === "/") {
+      return true;
+    }
+  };
+  const handleKeyPress = useCallback((event) => {
+    const num = event.key;
+    console.log(event);
+    if (isNumberPressed(num)) inputNumKey(event);
+    if (isOperatorPressed(num)) operatorTypekey(event);
+    if (event.key == "Enter") equalsKey(event);
+    if (event.key == "Escape") reset();
+  });
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyPress);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyPress);
+    };
+  }, [handleKeyPress]);
+
   const inputNum = (e) => {
+    console.log(curState);
     if (curState.includes(".") && e.target.innerText === ".") return;
 
     if (total) {
@@ -21,6 +61,18 @@ function App() {
     curState
       ? setCurState((pre) => pre + e.target.innerText)
       : setCurState(e.target.innerText);
+    setTotal(false);
+  };
+  // InputNum for keypress
+  const inputNumKey = (event) => {
+    if (curState.includes(".") && event.key === ".") return;
+
+    if (total) {
+      setPreState("");
+    }
+
+    curState ? setCurState((pre) => pre + event.key) : setCurState(event.key);
+    console.log(curState);
     setTotal(false);
   };
 
@@ -43,9 +95,49 @@ function App() {
       setCurState("");
     }
   };
+  // operatorType for Keypress
+  const operatorTypekey = (event) => {
+    setTotal(false);
+    setOperator(event.key);
+    if (curState === "") return;
+    if (preState !== "") {
+      equals();
+    } else {
+      setPreState(curState);
+      setCurState("");
+    }
+  };
 
   const equals = (e) => {
     if (e?.target.innerText === "=") {
+      setTotal(true);
+    }
+    console.log(operator);
+    let cal;
+    switch (operator) {
+      case "/":
+        cal = String(parseFloat(preState) / parseFloat(curState));
+        break;
+      case "+":
+        cal = String(parseFloat(preState) + parseFloat(curState));
+        break;
+      case "-":
+        cal = String(parseFloat(preState) - parseFloat(curState));
+        break;
+      case "x":
+        cal = String(parseFloat(preState) * parseFloat(curState));
+        break;
+      default:
+        return;
+    }
+    setInput("");
+    setPreState(cal);
+    setCurState("");
+  };
+
+  // equals for key
+  const equalsKey = (event) => {
+    if (event.key === "=" || event.key === "Enter") {
       setTotal(true);
     }
 
@@ -61,6 +153,9 @@ function App() {
         cal = String(parseFloat(preState) - parseFloat(curState));
         break;
       case "x":
+        cal = String(parseFloat(preState) * parseFloat(curState));
+        break;
+      case "*":
         cal = String(parseFloat(preState) * parseFloat(curState));
         break;
       default:
