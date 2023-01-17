49d96ecb9b94c37e93de115bd5796814eb8a3c18
radhikarungta <radhika.rr2002@gmail.com>
2023-01-17 16:10:45 +0530

    history feature added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import "./App.css";
 import { NumberFormatBase, NumericFormat } from "react-number-format";
 import { useState, useEffect, useCallback } from "react";
 import ReactCardFlip from "react-card-flip";
+
 function App() {
   const [flip, setFlip] = useState(false);
   const [preState, setPreState] = useState("");
+  const [calculation, setCalculation] = useState("");
+  const [calculationArray, setCalculationArray] = useState([]);
   const [curState, setCurState] = useState("");
-  const [curStateKey, setCurStateKey] = useState("");
   const [input, setInput] = useState("0");
   const [operator, setOperator] = useState(null);
   const [total, setTotal] = useState(false);
-
   const isNumberPressed = (key) => {
     if (
       key === "1" ||
@@ -28,7 +29,6 @@ function App() {
       return true;
     }
   };
-
   const isOperatorPressed = (key) => {
     if (key === "+" || key === "-" || key === "*" || key === "/") {
       return true;
@@ -36,11 +36,10 @@ function App() {
   };
   const handleKeyPress = useCallback((event) => {
     const num = event.key;
-    console.log(event);
     if (isNumberPressed(num)) inputNumKey(event);
     if (isOperatorPressed(num)) operatorTypekey(event);
-    if (event.key == "Enter") equalsKey(event);
-    if (event.key == "Escape") reset();
+    if (event.key === "Enter") equalsKey(event);
+    if (event.key === "Escape") reset();
   });
   useEffect(() => {
     document.addEventListener("keydown", handleKeyPress);
@@ -51,7 +50,6 @@ function App() {
   }, [handleKeyPress]);
 
   const inputNum = (e) => {
-    console.log(curState);
     if (curState.includes(".") && e.target.innerText === ".") return;
 
     if (total) {
@@ -72,13 +70,16 @@ function App() {
     }
 
     curState ? setCurState((pre) => pre + event.key) : setCurState(event.key);
-    console.log(curState);
     setTotal(false);
   };
 
   useEffect(() => {
     setInput(curState);
   }, [curState]);
+
+  useEffect(() => {
+    setCalculationArray(calculationArray);
+  }, [calculationArray]);
 
   useEffect(() => {
     setInput("0");
@@ -107,13 +108,12 @@ function App() {
       setCurState("");
     }
   };
-
   const equals = (e) => {
     if (e?.target.innerText === "=") {
       setTotal(true);
     }
-    console.log(operator);
     let cal;
+    var calculation;
     switch (operator) {
       case "/":
         cal = String(parseFloat(preState) / parseFloat(curState));
@@ -130,6 +130,27 @@ function App() {
       default:
         return;
     }
+    switch (operator) {
+      case "/":
+        calculation = preState + "/" + curState;
+        break;
+      case "+":
+        calculation = preState + "+" + curState;
+        break;
+      case "-":
+        calculation = preState + "-" + curState;
+        break;
+      case "x":
+        calculation = preState + "*" + curState;
+        break;
+      default:
+        return;
+    }
+    calculation = calculation + " = " + cal;
+    setCalculation(calculation);
+    calculationArray.push(calculation);
+    setCalculationArray(calculationArray);
+    setData(calculationArray);
     setInput("");
     setPreState(cal);
     setCurState("");
@@ -142,6 +163,7 @@ function App() {
     }
 
     let cal;
+    var calculation;
     switch (operator) {
       case "/":
         cal = String(parseFloat(preState) / parseFloat(curState));
@@ -161,6 +183,25 @@ function App() {
       default:
         return;
     }
+    switch (operator) {
+      case "/":
+        calculation = preState + "/" + curState;
+        break;
+      case "+":
+        calculation = preState + "+" + curState;
+        break;
+      case "-":
+        calculation = preState + "-" + curState;
+        break;
+      case "x":
+        calculation = preState + "*" + curState;
+        break;
+      default:
+        return;
+    }
+    calculation = calculation + " = " + cal;
+    setCalculation(calculation);
+    setData(calculation);
     setInput("");
     setPreState(cal);
     setCurState("");
@@ -171,92 +212,122 @@ function App() {
     setCurState("");
     setInput("0");
   };
+  const setData = (cal) => {
+    console.log(cal);
+    localStorage.setItem("data", cal);
+  };
+  const getData = () => {
+    let data = localStorage.getItem("data");
+    data = JSON.parse(data);
+    console.log(data);
+    return data;
+  };
+  const history = () => {
+    let data = getData();
+    setCalculationArray(data);
+  };
+  const renderList = calculationArray.map((item, index) => (
+    <div key={index}>{item}</div>
+  ));
+
+  const clear_h = () => {
+    setCalculationArray([]);
+  };
 
   return (
     <div className="u_container">
       <div className="container">
-        {/* <ReactCardFlip isFlipped={flip} flipDirection="vertical"> */}
-        <div className="wrapper">
-          <div className="screen">
-            <div className="input_container">
-              <div class="lcd-background">888888888888</div>
-              {input !== "" || input === "0" ? (
-                <NumericFormat
-                  value={input}
-                  displayType={"text"}
-                  thousandSeparator={false}
-                />
-              ) : (
-                <NumericFormat
-                  value={preState}
-                  displayType={"text"}
-                  thousandSeparator={false}
-                />
-              )}
+        <ReactCardFlip isFlipped={flip} flipDirection="vertical">
+          <div className="wrapper">
+            <div className="screen">
+              <div className="input_container">
+                <div class="lcd-background">888888888888</div>
+                {input !== "" || input === "0" ? (
+                  <NumericFormat
+                    value={input}
+                    displayType={"text"}
+                    thousandSeparator={false}
+                  />
+                ) : (
+                  <NumericFormat
+                    value={preState}
+                    displayType={"text"}
+                    thousandSeparator={false}
+                  />
+                )}
+              </div>
             </div>
+            <div className="btn" onClick={inputNum}>
+              7
+            </div>
+            <div className="btn" onClick={inputNum}>
+              8
+            </div>
+            <div className="btn" onClick={inputNum}>
+              9
+            </div>
+            <div className="btn orange" onClick={reset}>
+              C
+            </div>
+            <div className="btn" onClick={inputNum}>
+              4
+            </div>
+            <div className="btn" onClick={inputNum}>
+              5
+            </div>
+            <div className="btn" onClick={inputNum}>
+              6
+            </div>
+            <div className="btn" onClick={operatorType}>
+              -
+            </div>
+            <div className="btn" onClick={inputNum}>
+              1
+            </div>
+            <div className="btn" onClick={inputNum}>
+              2
+            </div>
+            <div className="btn" onClick={inputNum}>
+              3
+            </div>
+            <div className="btn" onClick={operatorType}>
+              +
+            </div>
+            <div className="btn" onClick={operatorType}>
+              /
+            </div>
+            <div className="btn zero" onClick={inputNum}>
+              0
+            </div>
+            <div className="btn" onClick={operatorType}>
+              x
+            </div>
+            <div className="btn" onClick={equals}>
+              =
+            </div>
+            <button
+              className="btn history"
+              onClick={() => {
+                setFlip(!flip);
+                history();
+              }}
+            >
+              History
+            </button>
           </div>
-          <div className="btn" onClick={inputNum}>
-            7
-          </div>
-          <div className="btn" onClick={inputNum}>
-            8
-          </div>
-          <div className="btn" onClick={inputNum}>
-            9
-          </div>
-          <div className="btn orange" onClick={reset}>
-            C
-          </div>
-          <div className="btn" onClick={inputNum}>
-            4
-          </div>
-          <div className="btn" onClick={inputNum}>
-            5
-          </div>
-          <div className="btn" onClick={inputNum}>
-            6
-          </div>
-          <div className="btn" onClick={operatorType}>
-            -
-          </div>
-          <div className="btn" onClick={inputNum}>
-            1
-          </div>
-          <div className="btn" onClick={inputNum}>
-            2
-          </div>
-          <div className="btn" onClick={inputNum}>
-            3
-          </div>
-          <div className="btn" onClick={operatorType}>
-            +
-          </div>
-          <div className="btn" onClick={operatorType}>
-            /
-          </div>
-          <div className="btn zero" onClick={inputNum}>
-            0
-          </div>
-          <div className="btn" onClick={operatorType}>
-            x
-          </div>
-          <div className="btn" onClick={equals}>
-            =
-          </div>
-          <button className="btn history" onClick={() => setFlip(!flip)}>
-            History
-          </button>
-        </div>
 
-        {/* <div className="container_flip">
+          <div className="container_flip">
             <div className="display_container">
-              <p>HISTORY</p>
+              <p>{renderList}</p>
             </div>
             <button className="btn back" onClick={() => setFlip(!flip)}>
               back
             </button>
+            <button className="btn" onClick={clear_h}>
+              clear
+            </button>
           </div>
-        </ReactCardFlip> */}
+        </ReactCardFlip>
       </div>
     </div>
   );
